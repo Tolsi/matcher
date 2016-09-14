71e0e06d00c031bc58392ce1de269b3721458e55
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-09-14 17:27:18 +0300

    And some more refactoring

--- src/main/scala/ru/tolsi/matcher/Matcher.scala
+++ src/main/scala/ru/tolsi/matcher/Matcher.scala
@@ -11,14 +11,16 @@ class Matcher(
     Order(id, creator, `type`, asset, price, qty)
   }
   def apply(orders: Seq[OrderOperation.Create])(implicit ec: ExecutionContext): Future[Unit] = {
-    val ordersFutures = orders.zipWithIndex.map { case (c, index) => createOrder(index, c) }.map(order => {
-      orderBook.addOrMatch(order).flatMap {
-        case Right(reverseOrders) =>
-          ordersExecutor.execute(reverseOrders, clientRepository)
-        case _ =>
-          Future.successful()
-      }
-    })
+    val ordersFutures = orders.view.zipWithIndex.map { case (c, index) => createOrder(index, c) }.map(executeOrder)
     Future.sequence(ordersFutures).map(_ => ())
+  }
+
+  private def executeOrder(order: Order): Future[Unit] = {
+    orderBook.addOrMatch(order).flatMap {
+      case Right(reverseOrders) =>
+        ordersExecutor.execute(reverseOrders, clientRepository)
+      case _ =>
+        Future.successful()
+    }
   }
 }
