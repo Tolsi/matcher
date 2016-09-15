e622cc0cd2df2f46eb13e263b6d86c330d0500cc
Sergey Tolmachev <tolsi.ru@gmail.com>
2016-09-15 11:13:02 +0300

    Open files by absolute path fix

--- src/main/scala/ru/tolsi/matcher/LoadExampleData.scala
+++ src/main/scala/ru/tolsi/matcher/LoadExampleData.scala
@@ -1,23 +1,24 @@
 package ru.tolsi.matcher
 
 import java.io.File
-import java.net.URI
 import com.typesafe.scalalogging.StrictLogging
 import ru.tolsi.matcher.util.EitherUtils._
 
+object LoadExampleData {
+  val defaultOrdersFilePath = "/orders.txt"
+  val defaultClientsFilePath = "/clients.txt"
+}
 trait LoadExampleData extends StrictLogging {
+  import LoadExampleData._
   def loadClients(pathOpt: Option[String] = None): Seq[ClientInfo] = {
     val (clientsErrors, clients) = splitEitherIterator(
-      ClientInfo.loadFromFile(
-        new File(
-          pathOpt.map(new URI(_)).getOrElse(getClass.getResource("/clients.txt").toURI))))
+      ClientInfo.loadFromFile(pathOpt.map(new File(_)).getOrElse(new File(getClass.getResource(defaultClientsFilePath).toURI))))
     clientsErrors.foreach(logger.warn(_))
     clients.toSeq
   }
   def loadCreateOrdersRequests(pathOpt: Option[String] = None): Seq[OrderOperation.Create] = {
     val (ordersErrors, createOrdersRequests) = splitEitherIterator(
-      OrderOperation.Create.loadFromFile(
-        new File(pathOpt.map(new URI(_)).getOrElse(getClass.getResource("/orders.txt").toURI))))
+      OrderOperation.Create.loadFromFile(pathOpt.map(new File(_)).getOrElse(new File(getClass.getResource(defaultOrdersFilePath).toURI))))
     ordersErrors.foreach(logger.warn(_))
     createOrdersRequests.toSeq
   }
